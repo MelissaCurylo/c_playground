43b32060a3db9a1fbd6c9db616a36e29bbdf7d98
Melissa <MelissaCode01@outlook.com>
2022-12-05 22:48:28 -0500

    Hello World

--- hello_world.c
+++ hello_world.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World!");
+    printf("Hello World!\n");
+    printf("I'm learning C.\n\n"); // two \n\n creates blank line.
+    printf("This will be my 6th computer language!!\nVery cool stuff."); // difficult to read on one line but possible.
+    return 0; // ends program.
 }
